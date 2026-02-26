c7ce8e6477844754729270e1565b00bd23dd4e85
lewelove <lewelove@proton.me>
2026-02-26 18:20:02 +0300

    2026-02-26 at 15:20 UTC

--- rust/src/compile/resolvers/mod.rs
+++ rust/src/compile/resolvers/mod.rs
@@ -1,26 +1,19 @@
-pub mod native;
 pub mod standard;
+pub mod native;
 
 use crate::compile::builder::context::{AlbumContext, TrackContext};
 use serde_json::{Value, json};
 
 pub fn resolve_album_key(key: &str, ctx: &AlbumContext) -> Option<Value> {
     match key {
-        "album" => Some(json!(standard::get_raw(
-            ctx.source,
-            "album",
-            "Unknown Album"
-        ))),
-        "albumartist" => Some(json!(standard::get_raw(
-            ctx.source,
-            "albumartist",
-            "Unknown Artist"
-        ))),
+        "album" => Some(json!(standard::get_raw(ctx.source, "album", "Unknown Album"))),
+        "albumartist" => Some(json!(standard::get_raw(ctx.source, "albumartist", "Unknown Artist"))),
         "date" => Some(json!(native::resolve_date(ctx))),
         "genre" => Some(json!(native::resolve_genre(ctx))),
         "comment" => Some(json!(native::resolve_comment(ctx))),
         "original_yyyy_mm" => Some(json!(native::resolve_yyyy_mm(ctx, "original_yyyy_mm"))),
         "release_yyyy_mm" => Some(json!(native::resolve_yyyy_mm(ctx, "release_yyyy_mm"))),
+        "custom_albumartist" => Some(json!(native::resolve_custom_albumartist(ctx))),
         "cover_chroma" => native::resolve_cover_chroma(ctx),
         "cover_entropy" => native::resolve_cover_entropy(ctx),
         _ => None,
