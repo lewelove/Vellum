09c5502e695e526934155b07163403206bb3d5ed
lewelove <lewelove@proton.me>
2026-03-25 20:50:34 +0300

    2026-03-25 at 17:50 UTC

--- config/extensions/javascript/functions/url.js
+++
@@ -1,7 +0,0 @@
-export function resolve_album_tag_discogs_url(ctx) {
-    return String(ctx.source.DISCOGS_URL || "");
-}
-
-export function resolve_album_tag_musicbrainz_url(ctx) {
-    return String(ctx.source.MUSICBRAINZ_URL || "");
-}
--- config/extensions/python/functions/url.py
+++
@@ -1,5 +0,0 @@
-def resolve_album_tag_discogs_url(ctx):
-    return str(ctx["source"].get("DISCOGS_URL", ""))
-
-def resolve_album_tag_musicbrainz_url(ctx):
-    return str(ctx["source"].get("MUSICBRAINZ_URL", ""))
