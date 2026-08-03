d78e1c8da979048508bad0446ea600a68bdb9cc6
lewelove <lewelove@proton.me>
2026-08-03 13:25:48 +0300

    configured interface linting

--- interfaces/web-app/eslint.config.js
+++ interfaces/web-app/eslint.config.js
@@ -1,37 +1,45 @@
-import sveltePlugin from 'eslint-plugin-svelte';
-import svelteParser from 'svelte-eslint-parser';
+import sveltePlugin from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import tsEslint from "typescript-eslint";
 
 export default [
-  ...sveltePlugin.configs['flat/recommended'],
-  ...sveltePlugin.configs['flat/prettier'],
+  ...sveltePlugin.configs["flat/recommended"],
+  ...sveltePlugin.configs["flat/prettier"],
   {
-    files: ['**/*.svelte'],
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsEslint.parser
+    }
+  },
+  {
+    files: ["**/*.svelte"],
     languageOptions: {
       parser: svelteParser,
+      parserOptions: {
+        parser: tsEslint.parser
+      }
     },
     rules: {
-      'svelte/button-has-type': 'error',
-      'svelte/no-unused-svelte-ignore': 'error',
-      'svelte/valid-compile': 'error',
-      'svelte/no-at-html-tags': 'warn',
-      'svelte/infinite-reactive-loop': 'error',
-      'svelte/no-dupe-use-directives': 'error',
-
-      // Auto-sorts component attributes, props, and directives
-      'svelte/sort-attributes': [
-        'error',
+      "svelte/button-has-type": "error",
+      "svelte/no-unused-svelte-ignore": "error",
+      "svelte/valid-compile": "error",
+      "svelte/no-at-html-tags": "warn",
+      "svelte/infinite-reactive-loop": "error",
+      "svelte/no-dupe-use-directives": "error",
+      "svelte/sort-attributes": [
+        "error",
         {
           order: [
-            'this',
-            'bind:this',
-            'slot',
-            'definition', // e.g. class, id
-            'PROPERTIES', // component props
-            'BINDINGS',   // bind:*
-            'FUNCTIONS',  // handlers
-            'ACTIONS',    // use:*
-            'TRANSITIONS',// transition:*, animate:*
-            'EVENTS'      // on:*
+            "this",
+            "bind:this",
+            "slot",
+            "definition",
+            "PROPERTIES",
+            "BINDINGS",
+            "FUNCTIONS",
+            "ACTIONS",
+            "TRANSITIONS",
+            "EVENTS"
           ]
         }
       ]
